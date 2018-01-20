af3113523607ea939ba5ca5f642aedf566db7323
imokrani <idir_mokrani@hotmail.fr>
2018-01-20 18:16:35 +0100

    Recuperation du projet TestGitPrj1 dans la branche EIC_BR2

--- TestGitPrj1/src/main/java/org/test/git/ClasseA.java
+++ TestGitPrj1/src/main/java/org/test/git/ClasseA.java
@@ -4,16 +4,16 @@ public class ClasseA {
 
 	public static void main(String[] args) {
         
-		//Instruction 1  Branhce EIC_BR1
-		//Instruction 2  Branhce EIC_BR1
-		//Instruction 3  Branhce EIC_BR1
-		//Instruction 4  Branhce EIC_BR1
-		//Instruction 5  Branhce EIC_BR1
-		//Instruction 6  Branhce EIC_BR1
-		//Instruction 7  Branhce EIC_BR1
-		//Instruction 8  Branhce EIC_BR1
-		//Instruction 9  Branhce EIC_BR1
-		//Instruction 10 Branhce EIC_BR1
+		//Instruction 1  Branhce EIC_BR2   modif 01 
+		//Instruction 2  Branhce EIC_BR2
+		//Instruction 3  Branhce EIC_BR2
+		//Instruction 4  Branhce EIC_BR2
+		//Instruction 5  Branhce EIC_BR2
+		//Instruction 6  Branhce EIC_BR2   modif 01 
+		//Instruction 7  Branhce EIC_BR2
+		//Instruction 8  Branhce EIC_BR2
+		//Instruction 9  Branhce EIC_BR2
+		//Instruction 10 Branhce EIC_BR2
 	}
 
 }
