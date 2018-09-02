f5586578623a89936994f6148dfa10d15d2b02db
imokrani2 <mokraniidir@gmail.com>
2018-09-02 14:56:17 +0200

    Ajouter un commentaire dans la branche EIC_BR1

--- TestGitPrj1/src/main/resources/data/ordreSql.sql
+++ TestGitPrj1/src/main/resources/data/ordreSql.sql
@@ -1,3 +1,3 @@
 -- Ajouter une colonne dans la table des users pour la gestion des roles.
--- Ajouter une commentaire dnas la branche EIC_BR2
+-- Ajouter une commentaire dnas les deux branches EIC_BR1 et 2
 ALTER TABLE users ADD COLUMN roles CHARACTER VARYING(255);
